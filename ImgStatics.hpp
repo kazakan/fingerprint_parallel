ecf4a346b47df0064140887438124a53e356b388
Jh Do <dojh99@gmail.com>
2023-05-31 11:31:03 +0900

    add: add comments to ImgStatics and MinutiaeDetector

--- ImgStatics.hpp
+++ ImgStatics.hpp
@@ -3,6 +3,10 @@
 #include "Img.hpp"
 #include "MatrixBuffer.hpp"
 
+/**
+ * @brief Class calculates some mathmetical statics from MatrixBuffer<BYTE>
+ *
+ */
 class ImgStatics {
   private:
     OclInfo oclInfo;
@@ -10,8 +14,41 @@ class ImgStatics {
 
   public:
     ImgStatics(OclInfo oclInfo, string source);
+
+    /**
+     * @brief Sum all the elements in buffer.
+     *        This copies result from gpu because needs of aggregation
+     *        across work groups. 
+     *
+     * @param src MatrixBuffer<BYTE> to calculate
+     * @return sum of elements
+     */
     float sum(MatrixBuffer<BYTE> &src);
+
+    /**
+     * @brief Get average of elements in buffer.
+     *        This copies result from gpu because needs of aggregation
+     *        across work groups. 
+     * @param src MatrixBuffer<BYTE> to calculate
+     * @return Average of elements
+     */
     float mean(MatrixBuffer<BYTE> &src);
+
+    /**
+     * @brief Get variance of elements in buffer.
+     *        This copies result from gpu because needs of aggregation
+     *        across work groups. 
+     * @param src MatrixBuffer<BYTE> to calculate
+     * @return Variance of elements
+     */
     float var(MatrixBuffer<BYTE> &src);
+
+    /**
+     * @brief Get Sum of x^2 in buffer.
+     *        This copies result from gpu because needs of aggregation
+     *        across work groups. 
+     * @param src MatrixBuffer<BYTE> to calculate
+     * @return Sum of x^2 in elements
+     */
     float squareSum(MatrixBuffer<BYTE> &src);
 };
--- MinutiaeDetector.hpp
+++ MinutiaeDetector.hpp
@@ -3,6 +3,10 @@
 #include "Img.hpp"
 #include "MatrixBuffer.hpp"
 
+/**
+ * @brief Class extracts minutiaes from preprocessed binary image.
+ * 
+ */
 class MinutiaeDetector {
   private:
     OclInfo oclInfo;
@@ -10,5 +14,11 @@ class MinutiaeDetector {
 
   public:
     MinutiaeDetector(OclInfo oclInfo, string source);
+    
+    /**
+     * @brief Calulates cross numbers per pixel.
+     * @param src MatrixBuffer<BYTE> to calculate
+     * @param dst MatrixBuffer that Result be saved.
+     */
     void applyCrossNumber(MatrixBuffer<BYTE> &src, MatrixBuffer<BYTE> &dst);
 };
