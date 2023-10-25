346e6e210d4f5d004241d6d671265b83423aea4c
U-KONKUK\s_dojh99 <dojh99@gmail.com>
2023-10-25 13:06:09 +0900

    test: add random generate cases in gaussian, dynamicThresholding, normalize

--- test/kernel_test.cpp
+++ test/kernel_test.cpp
@@ -1,12 +1,14 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <cmath>
 #include <cstdint>
 #include <tuple>
 
 #include "ImgStatics.hpp"
 #include "ImgTransform.hpp"
 #include "OclInfo.hpp"
+#include "random_case_generator.hpp"
 
 #define BYTE uint8_t
 
@@ -78,6 +80,46 @@ TEST(ImageTransformTest, Normalize) {
          {237, 163, 52, 65, 129, 218, 62, 148, 212},
          {190, 141, 67, 76, 118, 177, 74, 131, 173}}};
 
+    // create random data
+    RandomMatrixGenerator generator;
+
+    std::mt19937_64 gen(47);
+    std::uniform_int_distribution<int> meanDis(0, 255);
+    std::uniform_int_distribution<int> varDis(0, 5000);
+
+    const int nRandomCases = 3;
+    for (int randomCaseNo = 0; randomCaseNo < nRandomCases; ++randomCaseNo) {
+        tuple<int, int, vector<BYTE>> inputData =
+            generator.generateMatData(0, 255, 5, 5);
+
+        const int NC = std::get<0>(inputData);
+        const int NR = std::get<1>(inputData);
+        const vector<BYTE>& arr = std::get<2>(inputData);
+
+        const float mean0 = meanDis(gen);
+        const float var0 = varDis(gen);
+        float mean, var, tmp;
+
+        tmp = 0;
+        for (int value : arr) {
+            mean += value;
+            tmp += value * value;
+        }
+
+        mean /= arr.size();
+        var = tmp / arr.size() - mean * mean;
+
+        vector<BYTE> result(arr.size());
+
+        for (int i = 0; i < arr.size(); ++i) {
+            float delta = sqrtf(var0 * (arr[i] - mean) * (arr[i] - mean) / var);
+            int val = arr[i] > mean ? mean0 + delta : mean0 - delta;
+            result[i] = clamp(val, 0, 255);
+        }
+
+        datasets.push_back({mean0, var0, NC, NR, arr, result});
+    }
+
     auto test_one_pair = [&](normalize_datatype& data) {
         MatrixBuffer<BYTE> bufferOriginal(std::get<2>(data), std::get<3>(data),
                                           std::get<4>(data));
@@ -125,11 +167,68 @@ TEST(ImageTransformTest, DynamicThresholding) {
           0, 100, 100, 100, 100, 100, 0,  //
           0, 100, 100, 100, 100, 100, 0,  //
           0, 0,   0,   0,   0,   0,   0},
-         {0, 0, 0, 0, 0, 0, 0,  //
-          0, 1, 1, 1, 1, 1, 0,  //
-          0, 1, 0, 0, 0, 1, 0,  //
-          0, 1, 1, 1, 1, 1, 0,  //
-          0, 0, 0, 0, 0, 0, 0}}};
+         {0, 0,   0,   0,   0,   0,   0,  //
+          0, 255, 255, 255, 255, 255, 0,  //
+          0, 255, 0,   0,   0,   255, 0,  //
+          0, 255, 255, 255, 255, 255, 0,  //
+          0, 0,   0,   0,   0,   0,   0}}};
+
+    // create random data
+    RandomMatrixGenerator generator;
+
+    std::mt19937_64 gen(47);
+    std::uniform_int_distribution<int> halfBlockSizeDis(1, 3);
+    std::uniform_real_distribution<float> scaleDis(0.8, 1.2);
+
+    const int nRandomCases = 3;
+    for (int randomCaseNo = 0; randomCaseNo < nRandomCases; ++randomCaseNo) {
+        tuple<int, int, vector<BYTE>> inputData =
+            generator.generateMatData(0, 255, 5, 5);
+
+        const int NC = std::get<0>(inputData);
+        const int NR = std::get<1>(inputData);
+        const vector<BYTE>& arr = std::get<2>(inputData);
+
+        const int halfBlockSize = halfBlockSizeDis(gen);
+        const int blockSize = halfBlockSize * 2 + 1;
+        const float scale = scaleDis(gen);
+
+        const auto value = [&](int r, int c) -> const BYTE {
+            if (r < 0 || r >= NR || c < 0 || c >= NC) {
+                return 0;
+            }
+
+            return arr[NC * r + c];
+        };
+
+        const auto dynamicThresholdVal = [&](int idx) -> const BYTE {
+            const int r = idx / NC;
+            const int c = idx % NC;
+
+            float sum = 0;
+
+            for (int nextR = r - halfBlockSize; nextR <= r + halfBlockSize;
+                 ++nextR) {
+                for (int nextC = c - halfBlockSize; nextC <= c + halfBlockSize;
+                     ++nextC) {
+                    sum += value(nextR, nextC);
+                }
+            }
+
+            float mean = sum / ((blockSize * blockSize));
+            mean *= scale;
+
+            return value(r, c) > mean ? 255 : 0;
+        };
+
+        vector<BYTE> result(arr.size());
+
+        for (int i = 0; i < arr.size(); ++i) {
+            result[i] = dynamicThresholdVal(i);
+        }
+
+        datasets.push_back({blockSize, scale, NC, NR, arr, result});
+    }
 
     auto test_one_pair = [&](dynamic_thresholding_datatype& data) {
         MatrixBuffer<BYTE> bufferOriginal(std::get<2>(data), std::get<3>(data),
@@ -175,6 +274,54 @@ TEST(ImageTransformTest, ApplyGaussian) {
                                          0, 6,  13, 6,  0,  //
                                          0, 0,  0,  0,  0}}};
 
+    // create random data
+    RandomMatrixGenerator generator;
+    const int nRandomCases = 100;
+    for (int randomCaseNo = 0; randomCaseNo < nRandomCases; ++randomCaseNo) {
+        tuple<int, int, vector<BYTE>> inputData =
+            generator.generateMatData(0, 255, 5, 5);
+
+        const int NC = std::get<0>(inputData);
+        const int NR = std::get<1>(inputData);
+        const vector<BYTE>& arr = std::get<2>(inputData);
+
+        const auto value = [&](int r, int c) -> const BYTE {
+            if (r < 0 || r >= NR || c < 0 || c >= NC) {
+                return 0;
+            }
+
+            return arr[NC * r + c];
+        };
+
+        const int dx[] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+        const int dy[] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+        const int weight[] = {1, 2, 1, 2, 4, 2, 1, 2, 1};
+
+        const auto gaussianVal = [&](int idx) -> const BYTE {
+            const int r = idx / NC;
+            const int c = idx % NC;
+
+            BYTE ret = 0;
+            int sum = 0;
+
+            for (int i = 0; i < 9; ++i) {
+                sum += value(r + dx[i], c + dy[i]) * weight[i];
+            }
+
+            ret = (sum + 8) / 16;
+
+            return ret;
+        };
+
+        vector<BYTE> result(arr.size());
+
+        for (int i = 0; i < arr.size(); ++i) {
+            result[i] = gaussianVal(i);
+        }
+
+        datasets.push_back({NC, NR, arr, result});
+    }
+
     auto test_one_pair = [&](gaussian_datatype& data) {
         MatrixBuffer<BYTE> bufferOriginal(std::get<0>(data), std::get<1>(data),
                                           std::get<2>(data));
@@ -186,8 +333,7 @@ TEST(ImageTransformTest, ApplyGaussian) {
         bufferResult.createBuffer(oclInfo.ctx);
         bufferOriginal.toGpu(oclInfo);
 
-        imgTransformer.applyGaussianFilter(
-            bufferOriginal, bufferResult);
+        imgTransformer.applyGaussianFilter(bufferOriginal, bufferResult);
 
         bufferResult.toHost(oclInfo);
 
